0fa9a09d01560e86d7b9905d629284638a67f0fe
guanerye <lisongnan7@hotmail.com>
2017-05-30 22:39:57 +0900

    vcs20170530

--- pytest01.py
+++ pytest01.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+
+class Employee:
+    empCount = 0
+
+    def __init__(self,name,salary):
+        self.sss = name
+        self.eee = salary
+        Employee.empCount += 1
+
+
+    def displayCount(self):
+        print self.empCount
+
+    def displayEmployee(self):
+        print self.sss, self.eee
+
+
+em1 = Employee("leo",30)
+em1.displayCount()
+em2 = Employee("leo2",44)
+em2.displayCount()
