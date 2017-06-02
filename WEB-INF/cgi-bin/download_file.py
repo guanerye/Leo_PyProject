36cd118a031797b1740795bb5aac10cdc4e826f3
guanerye <lisongnan7@hotmail.com>
2017-06-02 16:56:02 +0900

    vcs20170530

--- WEB-INF/cgi-bin/download_file.py
+++ WEB-INF/cgi-bin/download_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-print "Content-Disposition: attachment; filename=\"foo2.txt\""
+print "Content-Disposition: attachment; filename=\"foo2333.txt\""
 print
 
 fo = open("/usr/local/apache-tomcat8/webapps/Leo_PyProject/WEB-INF/cgi-bin/foo2.txt","rb")
