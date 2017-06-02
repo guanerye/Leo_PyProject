1ccef0f5e2aa581de3e3652cf5791db887425b2b
guanerye <lisongnan7@hotmail.com>
2017-06-02 14:30:50 +0900

    vcs20170530

---
+++ WEB-INF/cgi-bin/dropdown.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 引入 CGI 处理模块
+import cgi, cgitb
+
+form = cgi.FieldStorage()
+
+if form.getvalue('dropdown'):
+    dropdown_value = form.getvalue('dropdown')
+else:
+    dropdown_value = 'nothing'
+
+print "Content-type:text/html"
+print
+print "<html>"
+print "<head>"
+print "<meta charset=\"utf-8\">"
+print "<title>菜鸟教程 CGI 测试实例</title>"
+print "</head>"
+print "<body>"
+print "<h2> 选中的选项是：%s</h2>" % dropdown_value
+print "</body>"
+print "</html>"
