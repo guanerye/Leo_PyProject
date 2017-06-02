3e5ed3d275d9075add5e570e0b3a5c381495eb57
guanerye <lisongnan7@hotmail.com>
2017-06-02 16:33:27 +0900

    vcs20170530

---
+++ WEB-INF/cgi-bin/save_file.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import cgi, os
+import cgitb; cgitb.enable()
+
+form = cgi.FieldStorage()
+
+filename = form['filename']
+
+if filename.filename:
+    fn = os.path.basename(filename.filename)
+    open('tmp' + fn, 'wb').write(filename.filename)
+    message = 'file "' + fn + '"upload OK'
+else:
+    message = 'file upload failed'
+
+print """
+Content-Type: text/html\n
+<html>
+<head>
+<meta charset="utf-8">
+<title>菜鸟教程(runoob.com)</title>
+</head>
+<body>
+   <p>%s</p>
+</body>
+</html>
+""" %(message,)
+
