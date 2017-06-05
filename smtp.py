3cef06334648fde10f2d40e154eadc3eac884384
guanerye <lisongnan7@hotmail.com>
2017-06-05 17:10:11 +0900

    vcs20170530

--- smtp.py
+++ smtp.py
@@ -5,20 +5,20 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
-sender = 'from@runoob.com'
-receivers = ['429240967@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+sender = 'leo@shabi.com'
+receivers = ['lisongnan7@hotmail.com']
 
-# 三个参数：第一个为文本内容，第二个 plain 设置文本格式，第三个 utf-8 设置编码
-message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
-message['From'] = Header("菜鸟教程", 'utf-8')
-message['To'] = Header("测试", 'utf-8')
+message = MIMEText('Python test email contents','plain', 'utf-8')
+message['From'] =Header("leo email",'utf-8')
+message['To'] = Header("Hello",'utf-8')
 
-subject = 'Python SMTP 邮件测试'
-message['Subject'] = Header(subject, 'utf-8')
+subject = "Python email test"
+message['Subject'] = Header(subject,'utf-8')
 
 try:
     smtpObj = smtplib.SMTP('localhost')
-    smtpObj.sendmail(sender, receivers, message.as_string())
-    print "邮件发送成功"
+    smtpObj.sendmail(sender,receivers,message.as_string())
+    print "email send succeed"
 except smtplib.SMTPException:
-    print "Error: 无法发送邮件"
+    print "Error: can not send the email"
+
